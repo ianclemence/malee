4e794f812835c04d55bd601d7ea4e5467e0bb216
Ian Clemence <batricupid@gmail.com>
2026-07-29 18:05:17 +0700

    remove sketchy components from git tracking

--- components/sketchy/index.ts
+++
@@ -1,7 +0,0 @@
-export { SketchyProvider, useSketchyDefaults } from './SketchyProvider';
-export type { SketchyDefaults } from './SketchyProvider';
-export { SketchyBox } from './SketchyBox';
-export { SketchyButton } from './SketchyButton';
-export { SketchyCircle } from './SketchyCircle';
-export { SketchyLine } from './SketchyLine';
-export { SketchyPath } from './SketchyPath';
