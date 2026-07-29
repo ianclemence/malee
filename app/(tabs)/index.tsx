49d4cc71e00a75aabbecbb49bb2d410ce5e4ab76
Ian Clemence <batricupid@gmail.com>
2026-07-29 18:02:10 +0700

    increase logo size to 40

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -322,6 +322,7 @@ const styles = StyleSheet.create({
   logo: {
     color: TEXT,
     fontFamily: 'RubikSprayPaint_400Regular',
+    fontSize: 40,
   },
   heroCard: {
     backgroundColor: Palette.black,
